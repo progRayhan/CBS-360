45d85e26b85f77f21c76d5a6007220241dca5853
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2026-04-20 16:27:15 +0600

    chore: modify customer table

--- cmd/models/customer.go
+++ cmd/models/customer.go
@@ -1,23 +1,24 @@
 package models
 
 type Customer struct {
-	ID               uint   `gorm:"primaryKey"`
-	CifNumber        string `gorm:"size:60;not null;unique"`
-	CustomerType     string `gorm:"size:10"`
-	CustomerName     string `gorm:"size:100"`
-	Address1         string `gorm:"type:text"`
-	Address2         string `gorm:"type:text"`
-	Country          string `gorm:"size:20"`
-	Sname            string `gorm:"size:40"`
-	Nationality      string `gorm:"size:30"`
-	LocalBranch      string `gorm:"size:5"`
-	CustomerCategory string `gorm:"size:20"`
-	FullName         string `gorm:"size:100"`
-	CifCreationDate  string `gorm:"size:30"`
-	ArApTracking     string `gorm:"size:5"` // Accounts Receivable / Accounts Payable Tracking
-	ExpectedCountry  string `gorm:"size:20"`
-	Frozen           string `gorm:"size:5;default:N"`
-	Dead             string `gorm:"size:5;default:N"`
-	Whrunkn          string `gorm:"size:5;default:N"`
-	Media            string `gorm:"size:20"`
+	ID                 uint   `gorm:"primaryKey"`
+	CifNumber          string `gorm:"size:60;not null;unique"`
+	CustomerType       string `gorm:"size:10"`
+	CustomerName       string `gorm:"size:100"`
+	Address1           string `gorm:"type:text"`
+	Address2           string `gorm:"type:text"`
+	Country            string `gorm:"size:20"`
+	Sname              string `gorm:"size:40"`
+	Nationality        string `gorm:"size:30"`
+	LocalBranch        string `gorm:"size:5"`
+	CustomerCategory   string `gorm:"size:20"`
+	FullName           string `gorm:"size:100"`
+	CifCreationDate    string `gorm:"size:30"`
+	ArApTracking       string `gorm:"size:5"` // Accounts Receivable / Accounts Payable Tracking
+	ExpectedCountry    string `gorm:"size:20"`
+	Frozen             string `gorm:"size:5;default:N"`
+	Dead               string `gorm:"size:5;default:N"`
+	Whrunkn            string `gorm:"size:5;default:N"`
+	Media              string `gorm:"size:20"`
+	KycReferenceNumber string `gorm:"size:40;not null;unique"`
 }
