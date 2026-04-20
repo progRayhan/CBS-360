70c7dd54ce4bf8ee1932aa3c88666140a2cb85e1
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2026-04-20 15:00:18 +0600

    chore: modify customer table

--- cmd/models/customer.go
+++ cmd/models/customer.go
@@ -1,13 +1,17 @@
 package models
 
 type Customer struct {
-	ID           uint   `gorm:"primaryKey"`
-	CifNumber    string `gorm:"size:60;not null"`
-	CustomerType string `gorm:"size:10;not null"`
-	CustomerName string `gorm:"size:100;not null"`
-	Address1     string `gorm:"type:text"`
-	Address2     string `gorm:"type:text"`
-	Country      string `gorm:"size:20"`
-	Sname        string `gorm:"size:40"`
-	Nationality  string `gorm:"size:30"`
+	ID               uint   `gorm:"primaryKey"`
+	CifNumber        string `gorm:"size:60;not null;unique"`
+	CustomerType     string `gorm:"size:10"`
+	CustomerName     string `gorm:"size:100"`
+	Address1         string `gorm:"type:text"`
+	Address2         string `gorm:"type:text"`
+	Country          string `gorm:"size:20"`
+	Sname            string `gorm:"size:40"`
+	Nationality      string `gorm:"size:30"`
+	LocalBranch      string `gorm:"size:5"`
+	CustomerCategory string `gorm:"size:20"`
+	FullName         string `gorm:"size:100"`
+	CifCreationDate  string `gorm:"size:30"`
 }
